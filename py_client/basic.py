d3d594ac39938f02f956422622e65cc103b5589e
Leonardo <leocordova96@gmail.com>
2023-12-12 14:27:05 -0300

    feat: product model

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.ge t(endpoint, params={"abc": 123}, json={"query": "hello"}) 
+get_response = requests.get(endpoint, json={"product_id": 123}) 
 # print(get_response.text)
 # print(get_response.status_code)
 
