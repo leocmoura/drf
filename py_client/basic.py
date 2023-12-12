7f4e541b49ffa7a9a59bd2b2c3a6830b3e778029
Leonardo <leocordova96@gmail.com>
2023-12-12 11:34:43 -0300

    pyclient

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,15 +2,11 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/api/" #http://127.0.0.1:8000/ 
+endpoint = "http://localhost:8000/"
 
-get_response = requests.get(endpoint, json={"product_id": 123 }) # HTTP Request
-# print(get_response.headers)
-# print(get_response.text) # print raw text response
-# print(get_response.status_code)
+get_response = requests.get(endpoint, json={"query": "Hello world"}) 
+print(get_response.text)
+print(get_response.status_code)
 
-# HTTP Request -> HTML
-# REST API HTTP Request -> JSON
-# JavaScript Object Nototion ~ Python Dict
-print(get_response.json())
+# print(get_response.json())
 # print(get_response.status_code)
