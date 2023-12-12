5fef24c86f61f160f67d3037fad2f735665e3ce8
Leonardo <leocordova96@gmail.com>
2023-12-12 11:35:39 -0300

    feat: create first view and urls

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,4 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django.http import JsonResponse
 
-from products.models import Product
-from products.serializers import ProductSerializer
-
-@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    """
-    DRF API View
-    """
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-        # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
-        data = ProductSerializer(instance).data
-    return Response(data)
+    return JsonResponse({"message" : "Hi ther, this is your Django API response"})
