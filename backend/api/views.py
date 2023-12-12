7c8703f999ecf599ae5baf0584c6e5b2c80f6560
Leonardo <leocordova96@gmail.com>
2023-12-12 12:30:35 -0300

    echo GET Data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,22 @@
+import json
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message" : "Hi ther, this is your Django API response"})
+    # request -> HttpRequest -> Django
+    # print(dir(request))
+    # request.body
+    print(request.GET)
+    print(request.POST)
+    body = request.body # byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    # print(request.headers)
+    # json.dumps(dict(request.headers))
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
