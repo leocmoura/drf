25ee511c51b3a02d590a0053e299bf66c6daa738
Leonardo <leocordova96@gmail.com>
2023-12-18 11:00:31 -0300

    Django Rest Framework Generics RetrieveAPIView

--- py_client/basic.py
+++ py_client/basic.py
@@ -7,7 +7,7 @@ import requests
 endpoint = "http://localhost:8000/api/post/"
 
 # get_response = requests.get(endpoint, json={'product_id': 123}) 
-get_response = requests.post(endpoint, json={'content': 'hello wooooooooooorld'}) 
+get_response = requests.post(endpoint, json={'title': 'Bigtitle', 'content': 'leozin hello', 'price': 'abc1234'}) 
 # print(get_response.headers)
 # print(get_response.text)
 # print(get_response.status_code)
