8f3eb3bcd7b2fc6955e798716b5f24e980be63f5
Leonardo <leocordova96@gmail.com>
2023-12-18 10:20:23 -0300

    update serializers field id

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -5,7 +5,7 @@ class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
-        fields = ['title', 'content', 'price', 'sale_price', 'my_discount']
+        fields = ['id', 'title', 'content', 'price', 'sale_price', 'my_discount']
 
     def get_my_discount(self, obj):
         if not hasattr(obj, 'id'):
